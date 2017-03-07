70f80afe14778fe2181d4e124a025eaa94b82e20
Mikołaj Słupiński <mikolajslupinski@gmail.com>
2017-03-07 21:33:47 +0100

    Some new ASK solutions

---
+++ ASK/Lista2/askl2z6.c
@@ -0,0 +1,27 @@
+#include "stdio.h"
+#include "stdint.h"
+
+int32_t comp(int32_t x, int32_t y){
+  //return ((x-y)^((x^y)&((x-y)^x)))>>31 & 0x00000001;
+  return ((x&(~y))|(~(x^y) & (x-y)))>>31 & 0x00000001; //oba poprawne
+}
+
+int32_t compu(uint32_t x, uint32_t y){
+  return ((~x&y)|(~(x^y)&(x-y)))>>31 & 0x00000001;
+}
+
+int main(){
+  int32_t a, b;
+  uint32_t ua, ub;
+  a = -1000;
+  b = 4234;
+  ua = 45;
+  ub = 4;
+
+
+  printf("%d\n", comp(a, b));
+  printf("%d\n", compu(ua, ub));
+
+  return 0;
+
+}
