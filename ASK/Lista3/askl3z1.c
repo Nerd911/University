0a623f898c72b9171fd30b949e4eb87022e4aa80
Mikołaj Słupiński <mikolajslupinski@gmail.com>
2017-03-14 20:17:53 +0100

    Some new ASK solutions

---
+++ ASK/Lista3/askl3z1.c
@@ -0,0 +1,1 @@
+//Jeżeli to czytasz nikczemniku, wziąłbyś się w końcu samemu do roboty
--- ASK/Lista3/askl3z2.c
+++ ASK/Lista3/askl3z2.c
@@ -1,8 +1,9 @@
 #include "stdio.h"
 #include "stdint.h"
+#include "limits.h"
 
 int32_t div3(int32_t n){
-  int64_t res = n + 1;
+  int64_t res = n + (n!=INT_MAX);
   res *= 0x0000000055555555;
   printf("%ld\n", res);
   return res >> 32;
