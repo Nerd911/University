7cd1b97c29d73ef630200952497327ea9c2354d3
Mikołaj Słupiński <mikolajslupinski@gmail.com>
2017-03-14 16:36:53 +0100

    Some new ASK solutions

--- ASK/Lista3/askl3z8.c
+++ ASK/Lista3/askl3z8.c
@@ -1,10 +1,18 @@
-#include <stdio.h>
+#include "stdio.h"
+#include "stdint.h"
 
-#define MAGIC_CONST 0x4B000000
+//2^23
+#define FRAC 0x4B000000
 
 int32_t float2int(int32_t f){
-
+  if(f >= 0)
+    return (f + FRAC) - FRAC;
+  return FRAC - (FRAC - f);
 }
 int main(){
+  float f = 32.9;
 
+  int32_t i = float2int(f);
+  printf("%d\n", i);
+  return 0;
 }
