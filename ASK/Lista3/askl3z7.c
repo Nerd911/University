6b020333074531a48c69bb56b9dafd8eb1428d31
Mikołaj Słupiński <mikolajslupinski@gmail.com>
2017-03-14 20:40:31 +0100

    Some new ASK solutions

--- ASK/Lista3/askl3z7.c
+++ ASK/Lista3/askl3z7.c
@@ -1,3 +1,55 @@
-x = x ^ 0x80000000; //zmiana znaku
+#include "stdio.h"
+#include "stdint.h"
+#include "string.h"
 
-(x==y)|((a|b) == 0x80000000); //x ==y
+
+//Porównywanie ze i bez znaku, patrz, lista 2 zad 6
+int32_t comp(int32_t x, int32_t y){
+  //return ((x-y)^((x^y)&((x-y)^x)))>>31 & 0x00000001;
+  return ((x&(~y))|(~(x^y) & (x-y)))>>31 & 0x00000001; //oba poprawne
+}
+
+int32_t compu(uint32_t x, uint32_t y){
+  return ((~x&y)|(~(x^y)&(x-y)))>>31 & 0x00000001;
+}
+
+int32_t floatbits(float f){
+  int32_t result;
+  memcpy(&result, &f, sizeof(result));
+  return result;
+}
+
+float bitsfloat(int32_t f){
+  float result;
+  memcpy(&result, &f, sizeof(result));
+  return result;
+}
+
+int32_t flipSign(int32_t x){
+  x = x ^ 0x80000000; //zmiana znaku
+  return x;
+}
+
+int32_t eq(int32_t x, int32_t y){
+  return (x==y)|((x|y) == 0x80000000); //x == y
+}
+
+int32_t compf(int32_t x, int32_t y){
+  return eq(x, y) | ~(x>>31)&comp(x,y) | (x>>31)&compu(y,x);
+}
+
+int main(){
+  float x, y;
+  x = 0.0;
+  y = 10.0;
+
+  int32_t ix, iy;
+  ix = floatbits(x);
+  iy = floatbits(y);
+
+  printf("%f\n", bitsfloat(flipSign(ix)));
+  printf("%d\n", eq(ix, iy));
+  printf("%d\n", compf(ix, iy));
+  printf("%f\n", bitsfloat(flipSign(ix)));
+
+}
